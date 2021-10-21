848686abbe946d36eb02443f8e85419bdb79db9b
Jen Kwon <70299766+jenkwon92@users.noreply.github.com>
2021-10-22 00:52:26 +0900

    DAO delete

--- src/main/java/org/kosta/project01/model/MemberDAO.java
+++ src/main/java/org/kosta/project01/model/MemberDAO.java
@@ -19,7 +19,6 @@ public class MemberDAO {
 	public static MemberDAO getInstance() {
 		return instance;
 	}
-	//closeAll 오버로딩 2개 만든다 
 	public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException {
 		if(pstmt!=null)
 			pstmt.close();
@@ -32,111 +31,10 @@ public class MemberDAO {
 		closeAll(pstmt, con);
 	}
 	public MemberVO findMemberById(String id) throws SQLException {
-		MemberVO vo=null;
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="select name,address from member where id=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs=pstmt.executeQuery();
-			if(rs.next())
-				vo=new MemberVO(id,null,rs.getString(1),rs.getString(2));
-		}finally {
-			closeAll(rs, pstmt, con);
-		}
-		return vo;
+		
+		return null;
 	}
-	public MemberVO login(String id,String password) throws SQLException {
-		MemberVO vo=null;
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="select name,address from member where id=? and password=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1, id);
-			pstmt.setString(2, password);
-			rs=pstmt.executeQuery();
-			if(rs.next())
-				vo=new MemberVO(id,password,rs.getString(1),rs.getString(2));
-		}finally {
-			closeAll(rs, pstmt, con);
-		}
-		return vo;
-	}
-	public ArrayList<MemberVO> findMemberListByAddress(String address) throws SQLException{
-		ArrayList<MemberVO> list=new ArrayList<MemberVO>();
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="select id,name from member where address=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1, address);
-			rs=pstmt.executeQuery();
-			while(rs.next()) {
-				list.add(new MemberVO(rs.getString(1),null,rs.getString(2),null));
-			}
-		}finally {
-			closeAll(rs, pstmt, con);
-		}
-		return list;
-	}
-	public void updateMember(MemberVO vo) throws SQLException{
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="update member set password=?, name=?,address=? where id=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1, vo.getPassword());
-			pstmt.setString(2, vo.getName());
-			pstmt.setString(3, vo.getAddress());
-			pstmt.setString(4, vo.getId());
-			pstmt.executeUpdate();
-		}finally {
-			closeAll(pstmt, con);
-		}
-	}
-	public void register(MemberVO vo) throws SQLException{
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="insert into member(id,password,name,address) values(?,?,?,?)";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1, vo.getId());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getName());
-			pstmt.setString(4, vo.getAddress());
-			pstmt.executeUpdate();
-		}finally {
-			closeAll(pstmt, con);
-		}
-	}
-	public boolean idcheck(String id) throws SQLException{
-		boolean  result=false;
-		Connection con=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		try {
-			con=DriverManager.getConnection(url, username, userpass);
-			String sql="select count(*) from member where id=?";
-			pstmt=con.prepareStatement(sql);
-			pstmt.setString(1,id);
-			rs=pstmt.executeQuery();
-			if(rs.next()&&rs.getInt(1)==1) // 조회정보가 1 이면 아이디가 존재하므로 true 할당 
-				result=true;
-		}finally {
-			closeAll(rs, pstmt, con);
-		}
-		return result;
-	}
+	
 }
 
 
